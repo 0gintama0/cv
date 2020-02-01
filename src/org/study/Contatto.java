c9d477a8b1288c647403dc019bb69883495ed4c3
Gabriele <Gabriele@GABIT001>
2020-02-01 18:53:59 +0100

    aggiornamento progetto master PerserXML

---
+++ src/org/study/Contatto.java
@@ -0,0 +1,51 @@
+package org.study;
+
+public class Contatto {
+
+	private int id;
+	private String nome;
+	private String cognome;
+	private String telefono;
+	private String email;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
